010745962ce17afcb2ed0d02bf4eba6ad2557ded
gladys-pascual <gladyskate@gmail.com>
2020-06-28 21:25:41 +0100

    fixing for build, href in footer and emoji in header components

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,17 +7,17 @@ const Footer = () => {
         <div className="footer-socials-section">
           <ul className="footer-socials">
             <li>
-              <a href="https://github.com/gladys-pascual/cheese-website" target="_blank">
+              <a href="https://github.com/gladys-pascual/cheese-website" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-github"></i>
               </a>
             </li>
             <li>
-              <a href="https://dev.to/gladyspascual" target="_blank">
+              <a href="https://dev.to/gladyspascual" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-dev"></i>
               </a>
             </li>
             <li>
-              <a href="https://www.linkedin.com/in/gladyspascual/" target="_blank">
+              <a href="https://www.linkedin.com/in/gladyspascual/" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-linkedin"></i>
               </a>
             </li>
@@ -26,8 +26,6 @@ const Footer = () => {
 
         <div className="go-to-top-parent">
           <a href="#" className="go-to-top">
-            {/* <i className="fab fa-angle-up"></i>
-            <br /> */}
             <span className="back-to-top">Back to Top</span>
           </a>
         </div>
--- src/Components/Heading.js
+++ src/Components/Heading.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Heading = (props) => {
   return (
     <header>
-      <h1>Welcome to my latte art journey ☕</h1>
+      <h1>Welcome to my latte art journey <span role="img" aria-label="coffee emoji">☕</span></h1>
       <h2> See my latte art progress: from a blob to something recognizable! </h2>
     </header>
   );
